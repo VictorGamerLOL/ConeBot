729d345fbba09cd606aab1ce698bcb85ef55847e
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-04-02 16:58:30 +0100

    Update bal.js

---
+++ src/commands/currency/bal.js
@@ -0,0 +1,18 @@
+const logger = require('../../util/logger');
+const db = require('../../util/mysqlinit')
+module.exports = {
+    name: 'bal',
+    description: 'Check your balance for a specific currency',
+    execute(message, args) {
+        if (args.length == 1) {
+            let [cur] = db.query(`SELECT * FROM ${message.guild.id}users WHERE userid = ${message.author.id}`)
+            if (cur.length == 0) {
+                message.channel.send(`You do not have any ${args[0]}`)
+            } else {
+                message.channel.send(`You have ${cur[0][args[0]]} ${args[0]}`)
+            }
+        } else {
+            message.channel.send(`You must provide a currency name`)
+        }
+    }
+}
