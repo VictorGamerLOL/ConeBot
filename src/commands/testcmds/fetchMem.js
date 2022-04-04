560d9ac3f6523eea433d8103906f859e74ed2f85
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-04-04 16:13:19 +0100

    Test command for fetching members
    Thanks discord for making this a privileged intent it took me an hour to debug very cool.

---
+++ src/commands/testcmds/fetchMem.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+var fs = require('fs');
+module.exports = {
+    name: 'fetchMem',   
+    aliases: ['fetch', 'fm'],
+    description: 'Fetches the members of a guild',
+    async execute (message) {
+        let members = await message.guild.members.fetch()
+        console.log(members)
+        message.channel.send(JSON.stringify(members))
+    }
+}
