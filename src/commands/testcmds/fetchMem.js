8a3b7fbc59f7fd082e71a4724481e4de52999ea6
VictorGamerLOL <51715529+VictorGamerLOL@users.noreply.github.com>
2022-04-04 18:31:03 +0100

    More Discord.js collection pain
    For real discord.js please update your documentation the methods don't even work from there wth.

--- src/commands/testcmds/fetchMem.js
+++ src/commands/testcmds/fetchMem.js
@@ -1,10 +1,15 @@
+const logger = require('../../utils/logger');
 module.exports = {
     name: 'fetchMem',   
     aliases: ['fetch', 'fm'],
     description: 'Fetches the members of a guild',
     async execute (message) {
-        let members = await message.guild.members.fetch()
+        let members = await message.guild.members.fetch({force: true})
         console.log(members)
         message.channel.send(JSON.stringify(members))
+        let someArray = Array.from(members.values())
+        logger.info("Split line")
+        console.log(someArray[0])
+        message.channel.send(JSON.stringify(someArray[0].user))
     }
 }
