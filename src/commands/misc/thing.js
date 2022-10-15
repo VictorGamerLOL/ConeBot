0711ab33abc5497273cbdc84a7f754fcd01b6271
VictorGamerLOL <hackandgo3@gmail.com>
2022-10-15 16:20:49 +0100

    deleted things & updated dependencies

--- src/commands/misc/thing.js
+++
@@ -1,8 +0,0 @@
-const logger = require("../../utils/logger")
-module.exports = {
-    name: 'thing',
-    description: 'What is that?',
-    execute(message,args) {
-        message.channel.send('something');
-    }
-}
